bd1924a14353a7812f3b524da64ee5229108765c
Pierre Hanlet <pdhanlet@gmail.com>
2022-02-03 00:09:25 -0600

    Added comments in various files

--- server/router.ts
+++ server/router.ts
@@ -3,14 +3,20 @@ import {read, write} from "./ymlController";
 
 export const router = express.Router();
 
+// Homepage. This is where students will view bus information from. 
+// Reads from data file and displays data
 router.get("/", (req: Request, res: Response) => {
     res.render("index", {data: read()});
 });
 
+// Admin page. This is where bus information can be updated from
+// Reads from data file and displays data
 router.get("/admin", (req: Request, res: Response) => {
     res.render("admin", {data: read()});
 });
 
+// Post request to update bus information. 
+// Writes to data file with the information provided by the form then redirects back to admin
 router.post("/api/save", (req: Request, res: Response) => {
     write(req.body);
     res.redirect("/admin");
--- server/ymlController.ts
+++ server/ymlController.ts
@@ -6,9 +6,11 @@ const filepath = path.resolve(__dirname, "../data/busData.yml");
 
 type Bus = {number: string, change: string, status: string};
 
-// Load data file
+// Load data file. If no file exists creates one
 export function read() {
+    // Checks if datafile exists
     if (!fs.existsSync(filepath)) {
+        // Creates a data directory if none exists
         if (!fs.existsSync(path.resolve(__dirname, "../data"))) {
             fs.mkdirSync(path.resolve(__dirname, "../data"));
         }
@@ -19,12 +21,13 @@ export function read() {
     change: ''
     status: 'NOT HERE'
 weather: ''`;  
+        // Creates data file
         fs.writeFileSync(filepath, data);
     }
     return <Bus[]> yaml.load(fs.readFileSync(filepath, "utf-8"));
 }
 
-// Edit data object
+// Writes to data file bus first formating the arrays provided by the form and then combining it with weather
 export function write(data: {
     busNumber: string | string[], 
     busChange: string | string[], 
@@ -32,12 +35,14 @@ export function write(data: {
     weather: string}
     ) {
     const buses: Bus[] = [];
+    // In case of one bus
     if (typeof data.busNumber === "string" && 
         typeof data.busChange === "string" && 
         typeof data.busStatus === "string"
     ) {
         buses.push({number: data.busNumber, change: data.busChange, status: data.busStatus});
     }
+    // In case of multiple buses
     else {
         for(let i = 0; i < data.busNumber.length; i++) {
             buses.push({number: data.busNumber[i], change: data.busChange[i], status: data.busStatus[i]});
