f6f04c3860774bb7b78d1823cab1dadfab3c436a
jepiar <jepiar@stu.naperville203.org>
2022-02-16 13:50:17 -0600

    comments were added to main

--- server/router.js
+++ server/router.js
@@ -7,12 +7,18 @@ exports.router = void 0;
 const express_1 = __importDefault(require("express"));
 const ymlController_1 = require("./ymlController");
 exports.router = express_1.default.Router();
+// Homepage. This is where students will view bus information from. 
+// Reads from data file and displays data
 exports.router.get("/", (req, res) => {
     res.render("index", { data: (0, ymlController_1.read)() });
 });
+// Admin page. This is where bus information can be updated from
+// Reads from data file and displays data
 exports.router.get("/admin", (req, res) => {
     res.render("admin", { data: (0, ymlController_1.read)() });
 });
+// Post request to update bus information. 
+// Writes to data file with the information provided by the form then redirects back to admin
 exports.router.post("/api/save", (req, res) => {
     (0, ymlController_1.write)(req.body);
     res.redirect("/admin");
--- static/ts/manageBuses.js
+++ static/ts/manageBuses.js
@@ -1,23 +1,28 @@
 "use strict";
+// Adds a new bus to the admin table
 function addBus() {
     const table = document.getElementById("table");
+    // Adds table row and cells
     const row = table.insertRow(1);
     const busNumber = row.insertCell(0);
     const busChange = row.insertCell(1);
     const busStatus = row.insertCell(2);
     const deleteBus = row.insertCell(3);
     deleteBus.setAttribute("class", "deleteBus");
+    // Creates input element for bus number and adds it to the first cell
     const numberInput = document.createElement("input");
     numberInput.setAttribute("class", "tableInput");
     numberInput.setAttribute("type", "number");
     numberInput.setAttribute("name", "busNumber");
     numberInput.setAttribute("required", "");
     busNumber.appendChild(numberInput);
+    // Creates input element for bus change and adds it to the second cell
     const changeInput = document.createElement("input");
     changeInput.setAttribute("class", "tableInput");
     changeInput.setAttribute("type", "number");
     changeInput.setAttribute("name", "busChange");
     busChange.appendChild(changeInput);
+    // Creates select element for bus status and adds it to the third cell
     const statusSelect = document.createElement("select");
     statusSelect.setAttribute("name", "busStatus");
     ["NOT HERE", "HERE", "LOADING", "GONE"].forEach((status) => {
@@ -26,11 +31,13 @@ function addBus() {
         statusSelect.appendChild(option);
     });
     busStatus.appendChild(statusSelect);
+    // Creates delete button and adds it to the fourth cell
     const deleteIcon = document.createElement("i");
     deleteIcon.setAttribute("class", "fas fa-times");
     deleteIcon.setAttribute("onclick", "removeBus(this)");
     deleteBus.appendChild(deleteIcon);
 }
+// Deletes a bus from the admin table
 function removeBus(icon) {
     icon.parentElement.parentElement.remove();
 }
