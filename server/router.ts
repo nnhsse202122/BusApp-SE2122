c0bff0a2efd3ab9651b30a2c3dd42c47a07385a0
Pierre Hanlet <pdhanlet@gmail.com>
2022-04-03 01:21:03 -0500

    Fixed whatever problem was added by npm updating req.session

--- server/router.ts
+++ server/router.ts
@@ -31,27 +31,27 @@ router.post("/auth/v1/google", async (req: Request, res: Response) => {
         idToken: token,
         audience: CLIENT_ID
     });
-    req.session!.userEmail = <string> (<TokenPayload> ticket.getPayload()).email; // Store email in session
+    req.session.userEmail = ticket.getPayload()!.email!; // Store email in session
     res.status(201).end();
 });
 
 // Checks if the user's email is in the whitelist and authorizes accordingly
 function authorize(req: Request) {
-    req.session!.isAdmin = readWhitelist().admins.includes(req.session!.userEmail); 
+    req.session.isAdmin = readWhitelist().admins.includes(<string> req.session.userEmail); 
 }
 
 /* Admin page. This is where bus information can be updated from
 Reads from data file and displays data */
 router.get("/admin", async (req: Request, res: Response) => {
     // If user is not authenticated (email is not is session) redirects to login page
-    if (!req.session!.userEmail) {
+    if (!req.session.userEmail) {
         res.redirect("/login");
         return;
     }
     
     // Authorizes user, then either displays admin page or unauthorized page
     authorize(req);
-    if (req.session!.isAdmin) {
+    if (req.session.isAdmin) {
         res.render("admin", {
             data: readData(),
             render: fs.readFileSync(path.resolve(__dirname, "../views/include/adminContent.ejs"))
