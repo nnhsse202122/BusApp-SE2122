cef982d8aab2464df2e9ecf1da87316a96d85ee1
Pierre Hanlet <pdhanlet@gmail.com>
2022-05-10 22:29:04 -0500

    More comments

--- static/ts/socket-io-client.d.ts
+++ static/ts/socket-io-client.d.ts
@@ -3,6 +3,7 @@
 import {io, Socket} from "socket.io-client";
 export {};
 
+// Tell typescript io is an attribute of the window (even though it's not) so that it doesn't complain
 declare global {
     interface Window {
         io: typeof io;
