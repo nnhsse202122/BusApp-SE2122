b7122024a98a5997a3c43ca0b8320de8336cf599
Pierre Hanlet <pdhanlet@gmail.com>
2022-12-19 08:19:12 -0600

    Added a bunch of functionality to update bus list page

--- static/ts/updateBusList.js
+++ static/ts/updateBusList.js
@@ -1,14 +1,30 @@
 "use strict";
 // /// <reference path="./socket-io-client.d.ts"/>
 // const updateBusListSocket = window.io('/updateBusList');
+// class Bus_busList {
+//     row: HTMLTableRowElement;
+//     removeIcon: HTMLElement;
+//     number: string | undefined;
+//     constructor(rowVal: HTMLTableRowElement) {
+//         this.row = rowVal;      
+//         this.removeIcon = <HTMLElement> this.row.children[4].children[0];
+//         this.data = {} as BusData;
+//     }
+// }
 let busList;
 fetch("/busList").then((res) => res.json()).then((data) => { busList = data; console.log(busList); });
-// console.log(busList!)
 let newBusEmptyRow;
 fetch("/updateBusListEmptyRow").then((res) => res.text()).then((data) => newBusEmptyRow = data);
 function newBus_busList() {
     const row = document.getElementsByClassName("buslist-table")[0].insertRow(2);
     const html = ejs.render(newBusEmptyRow);
     row.innerHTML = html;
+    row.children[0].children[0].focus();
+}
+function removeBus_busList(removeIcon) {
+    let row = removeIcon.parentElement.parentElement;
+    let number = row.children[0].innerHTML;
+    busList.splice(busList.indexOf(number), 1);
+    row.remove();
 }
 //# sourceMappingURL=updateBusList.js.map
--- static/ts/updateBusList.ts
+++ static/ts/updateBusList.ts
@@ -2,9 +2,20 @@
 
 // const updateBusListSocket = window.io('/updateBusList');
 
-let busList: number[];
+// class Bus_busList {
+//     row: HTMLTableRowElement;
+//     removeIcon: HTMLElement;
+//     number: string | undefined;
+
+//     constructor(rowVal: HTMLTableRowElement) {
+//         this.row = rowVal;      
+//         this.removeIcon = <HTMLElement> this.row.children[4].children[0];
+//         this.data = {} as BusData;
+//     }
+// }
+
+let busList: string[];
 fetch("/busList").then((res) => res.json()).then((data) => {busList = data; console.log(busList)});
-// console.log(busList!)
 
 let newBusEmptyRow: string;
 fetch("/updateBusListEmptyRow").then((res) => res.text()).then((data) => newBusEmptyRow = data);
@@ -13,4 +24,13 @@ function newBus_busList() {
     const row = (<HTMLTableElement> document.getElementsByClassName("buslist-table")[0]).insertRow(2);
     const html = ejs.render(newBusEmptyRow);
     row.innerHTML = html;
+
+    (row.children[0]!.children[0] as HTMLInputElement).focus()
+}
+
+function removeBus_busList(secondChild: HTMLElement) {
+    let row = secondChild.parentElement!.parentElement! as HTMLTableRowElement;
+    let number = row.children[0]!.innerHTML;
+    busList.splice(busList.indexOf(number), 1);
+    row.remove();
 }
